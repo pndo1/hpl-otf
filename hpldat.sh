856430408f24b5e94bbd44539c90b3c97201038a
Ryan <Ryan@Ryans-MacBook-Pro.local>
2017-07-25 16:55:15 -0500

    Ns handling

--- hpldat.sh
+++ hpldat.sh
@@ -17,6 +17,37 @@ echo $hplfolder $core
 mv $hplfolder $core
 cd $core
 }
+edit_hpldat () {
+  corex=$1
+  if [[ -n "$ns" ]]; then
+    sed -i '6s/.*/'$ns'/' HPL.dat
+  fi
+  if [[ "$corex" == "1" ]]; then
+    sed -i '11s/.*/1/' HPL.dat
+    sed -i '12s/.*/1/' HPL.dat
+  elif [[ "$corex" == "2" ]]; then
+    sed -i '11s/.*/2/' HPL.dat
+    sed -i '12s/.*/1/' HPL.dat
+  elif [[ "$corex" == "4" ]]; then
+    sed -i '11s/.*/2/' HPL.dat
+    sed -i '12s/.*/2/' HPL.dat
+  elif [[ "$corex" == "6" ]]; then
+    sed -i '11s/.*/3/' HPL.dat
+    sed -i '12s/.*/2/' HPL.dat
+  elif [[ "$corex" == "12" ]]; then
+    sed -i '11s/.*/4/' HPL.dat
+    sed -i '12s/.*/3/' HPL.dat
+  elif [[ "$corex" == "24" ]]; then
+    sed -i '11s/.*/6/' HPL.dat
+    sed -i '12s/.*/4/' HPL.dat
+  elif [[ "$corex" == "36" ]]; then
+    sed -i '11s/.*/6/' HPL.dat
+    sed -i '12s/.*/6/' HPL.dat
+  elif [[ "$corex" == "48" ]]; then
+    sed -i '11s/.*/6/' HPL.dat
+    sed -i '12s/.*/8/' HPL.dat
+  fi
+}
 create_sjob () {
   cd $hplbinpathvar
   if [[ ! -d "scripts" ]]; then
@@ -40,10 +71,11 @@ create_sjob () {
   cd scripts/$nodes/$scale
   touch hpl-$core.sjob
   echo -e "#!/bin/bash\n#SBATCH -p pinnacle\n#SBATCH -t 12:00\n#SBATCH -N$1 -n$core\n#SBATCH --profile=all" >> hpl-$core.sjob
-  echo "#SBATCH -o $hplbinpathvar/results/$nodes/$scale/hpl-$core.out" >> hpl-$core.sjob
+  echo "#SBATCH -o $hplbinpathvar/results/$nodes/$scale/hpl-$core-'$SLURM_JOB_ID'.out" >> hpl-$core.sjob
   echo "export MODULEPATH=$MODULEPATH:/soft/modules" >> hpl-$core.sjob
   echo -e "module load compilers/intel\nmodule load blas/intel-mkl\nmodule load mpi/intel"  >> hpl-$core.sjob
   echo "cd $hplbinpathvar/$nodes/$scale/$core"  >> hpl-$core.sjob
+  echo "export I_MPI_PMI_LIBRARY=/usr/lib64/libpmi.so" hpl-$core.sjob
   echo "mpirun -n $core ./xhpl"  >> hpl-$core.sjob
 }
 
@@ -81,32 +113,10 @@ if [[ -d "$nodes" ]]; then
 fi
 
 if [[ "$nodes" == "single" ]]; then
-  export cores='1 2 4 6 12 24 48'
+  export cores='1 2 4 6 12 24 36 48'
 for core in $cores; do
   create_hpl
-
-  if [[ "$core" == "1" ]]; then
-    sed -i '11s/.*/1/' HPL.dat
-    sed -i '12s/.*/1/' HPL.dat
-  elif [[ "$core" == "2" ]]; then
-    sed -i '11s/.*/2/' HPL.dat
-    sed -i '12s/.*/1/' HPL.dat
-  elif [[ "$core" == "4" ]]; then
-    sed -i '11s/.*/2/' HPL.dat
-    sed -i '12s/.*/2/' HPL.dat
-  elif [[ "$core" == "6" ]]; then
-    sed -i '11s/.*/3/' HPL.dat
-    sed -i '12s/.*/2/' HPL.dat
-  elif [[ "$core" == "12" ]]; then
-    sed -i '11s/.*/4/' HPL.dat
-    sed -i '12s/.*/3/' HPL.dat
-  elif [[ "$core" == "24" ]]; then
-    sed -i '11s/.*/6/' HPL.dat
-    sed -i '12s/.*/4/' HPL.dat
-  elif [[ "$core" == "48" ]]; then
-    sed -i '11s/.*/8/' HPL.dat
-    sed -i '12s/.*/6/' HPL.dat
-  fi
+  edit_hpldat $core
 done
 for core in $cores; do
  create_sjob 1
